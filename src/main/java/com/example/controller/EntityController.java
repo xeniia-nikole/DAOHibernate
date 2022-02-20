0c2207216a3026acc8b490062ccf9f1c888ee06d
Kseniia <xenia-nikole@mail.ru>
2022-02-20 13:52:46 +0300

    add username authentication

--- src/main/java/com/example/controller/EntityController.java
+++ src/main/java/com/example/controller/EntityController.java
@@ -2,10 +2,14 @@ package com.example.controller;
 
 import com.example.model.Person;
 import com.example.service.EntityService;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,17 +23,25 @@ public class EntityController {
 
 
     @GetMapping("/persons/by-city")
+    @Secured("ROLE_READ")
     public List<Person> getNameByCity(@RequestParam("city") String city) {
         return service.findByCityOfLiving(city);
     }
 
     @GetMapping("/persons/by-person")
+    @RolesAllowed("ROLE_WRITE")
     public Optional<Person> getPersonByNameAndSurname(@RequestParam("name") String name,
                                                       @RequestParam("surname") String surname) {
         return service.findPersonByPersonId_NameAndAndPersonId_Surname(name, surname);
     }
     @GetMapping("/persons/by-age")
+    @PreAuthorize("hasRole('ROLE_WRITE') or hasRole('ROLE_DELETE')")
     public List<Person> getPersonsByAge(@RequestParam("age") int age) {
         return service.findAllByPersonId_AgeLessThanOrderByPersonId_Age(age);
+    }
+    @GetMapping("/username")
+    @PostAuthorize("#username == authentication.principal.username")
+    public String named(String username) {
+        return username + " entered the building.";
     }
 }
--- src/main/java/com/example/security/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -2,32 +2,34 @@ package com.example.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true,jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("Billy")
                 .password("{noop}qwerty")
-                .roles("USER");
+                .roles("READ");
         auth.inMemoryAuthentication()
                 .withUser("NoName")
                 .password("{noop}1234")
-                .roles("USER");
+                .roles("WRITE");
         auth.inMemoryAuthentication()
                 .withUser("Linus")
                 .password("{noop}lol")
-                .roles("USER");
+                .roles("DELETE");
 
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/persons/by-person", "/persons/by-city").permitAll()
+                .antMatchers("/persons/by-person","/persons/by-city").permitAll()
                 .and()
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
